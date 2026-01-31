f831b9fed476f8b13f4c54ab715db2e00e2801b5
taind <nguyendutai10011999@gmail.com>
2026-01-31 15:53:58 +0900

    first set up config tailwindcss with vite and vue3 setting docker environment

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,6 @@
 <template>
-  <h1>Event Manager</h1>
+  <h6 class="text-3xl font-bold text-red-500">Event Manager</h6>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import { api } from './services/api'
-
-onMounted(async () => {
-  const res = await api.get('/health')
-  console.log(res.data)
-})
 </script>
